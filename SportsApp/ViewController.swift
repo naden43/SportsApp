b1115da04b885828e83333ae940fd33a6d149074
Salma Maher <153623793+salmamaher14@users.noreply.github.com>
2024-05-11 22:54:24 +0300

    Finishing Leagues Details Screen Design

--- SportsApp/ViewController.swift
+++ SportsApp/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnFav: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Remove the border of btnFav
+        btnFav.layer.borderWidth = 0
+        btnFav.layer.borderColor = UIColor.clear.cgColor
     }
-
-
 }
+
 
